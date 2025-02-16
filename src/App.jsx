fc95d2993e87f1d866eaabd705eb46197807da0e
Đạt Đào Văn <150135661+daovandat119@users.noreply.github.com>
2025-02-16 16:46:00 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './pages/About';
-import Home from './pages/Home';
-import TopPicks from './pages/TopPicks';
-import BlogTopPicks from './pages/BlogTopPicks';
-import Topics from './pages/Topics';
-import BlogTopics  from './pages/BlogTopics';
-import ArticleCategory from './components/ArticleCategory';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import TopPicks from "./pages/TopPicks";
+import BlogTopPicks from "./pages/BlogTopPicks";
+import Topics from "./pages/Topics";
+import BlogTopics from "./pages/BlogTopics";
+import ArticleCategory from "./components/ArticleCategory";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <Router>
       <div className="container mx-auto">
         <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/top-picks" element={<TopPicks />} />
-              <Route path="/bog-top-picks" element={<BlogTopPicks />} />
-              <Route path="/topics" element={<Topics />} />
-              <Route path="/blog-topics" element={<BlogTopics />} />
-              <Route path="/article-category" element={<ArticleCategory />} />
-            </Routes>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/top-picks" element={<TopPicks />} />
+          <Route path="/bog-top-picks" element={<BlogTopPicks />} />
+          <Route path="/topics" element={<Topics />} />
+          <Route path="/blog-topics" element={<BlogTopics />} />
+          <Route path="/article-category" element={<ArticleCategory />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
